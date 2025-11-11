3bb5f311e34762a9bcffe32714310619599db0bd
clouwud <dakshgajbhiye902@gmail.com>
2025-11-11 19:22:33 +0530

    init

--- modules/hyprland/hyprland.nix
+++ modules/hyprland/hyprland.nix
@@ -9,8 +9,10 @@
   wayland.windowManager.hyprland.settings = {
     monitor = ",preferred,auto,1";
 
-    "$terminal" = "wezterm";
-    "$fileManager" = "wezterm start -- yazi";
+    # "$terminal" = "wezterm";
+    # "$fileManager" = "wezterm start -- yazi";
+    "$terminal" = "foot";
+    "$fileManager" = "foot -- yazi";
     "$menu" = "wmenu-run -N 282828 -n ebdbb2 -S ebdbb2 -s 282828 -f 'Iosevka Nerd Font 12' ";
     "$browser" = "firefox";
     "$volume_control" = "pavucontrol";
