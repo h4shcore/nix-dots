b528c26bb6a5cbb41b98d5798896864e35a7eaa3
h4shcore <>
2026-02-10 01:46:28 +0530

    init

--- modules/nvim/plugins/lualine.nix
+++ modules/nvim/plugins/lualine.nix
@@ -11,17 +11,42 @@
             component_separators = "";
             section_separators = "";
           };
+          # sections = {
+          #   lualine_a = [ "mode" ];
+          #   lualine_b = [ "branch" ];
+          #   lualine_c = [ "filename" ];
+          #   lualine_x = [ "lsp_status" ];
+          #   lualine_y = [
+          #     "searchcount"
+          #     "selectioncount"
+          #   ];
+          #   lualine_z = [ "location" ];
+          # };
+
           sections = {
-            lualine_a = [ "mode" ];
-            lualine_b = [ "branch" ];
-            lualine_c = [ "filename" ];
-            lualine_x = [ "lsp_status" ];
-            lualine_y = [
-              "searchcount"
-              "selectioncount"
-            ];
-            lualine_z = [ "location" ];
-          };
+          lualine_a = [ "mode" ];
+          lualine_b = [ "branch" ];
+          lualine_c = [ "filename" ];
+          lualine_x = [
+            {
+              __raw = ''
+                function()
+                  local encoding = vim.o.fileencoding
+                  if encoding == "" then
+                    return vim.bo.fileformat .. " :: " .. vim.bo.filetype
+                  else
+                    return encoding .. " :: "
+                      .. vim.bo.fileformat .. " :: "
+                      .. vim.bo.filetype
+                  end
+                end
+              '';
+            }
+          ];
+          lualine_y = [ "progress" ];
+          lualine_z = [ "location" ];
+        };
+
         };
       };
     };
