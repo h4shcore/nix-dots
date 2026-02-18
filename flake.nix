855e570d7cfaaac3ffc2367d2a2b8f32b6b9dde7
h4shcore <>
2026-02-18 12:11:17 +0530

    init

--- flake.nix
+++ flake.nix
@@ -2,13 +2,13 @@
   description = "dotfiles";
 
   inputs = {
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-unstable";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
+      # url = "github:nix-community/nixvim/nixos-25.11";
       url = "github:nix-community/nixvim/nixos-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
