ea082e79a92f93190558f3e81dc11a4a89973cd0
h4shcore <>
2026-02-18 12:06:32 +0530

    init

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   description = "dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-unstable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
@@ -12,8 +13,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nix-colors.url = "github:misterio77/nix-colors";
-    astal.url = "github:aylur/astal";
-    ags.url = "github:aylur/ags"; 
   };
 
   outputs =
