14869eea41e61e9c21e0e78ef05fdd8168281ede
h4shcore <dakshgajbhiye902@gmail.com>
2026-02-25 13:57:32 +0530

    init

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     # nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
       url = "github:nix-community/home-manager";
