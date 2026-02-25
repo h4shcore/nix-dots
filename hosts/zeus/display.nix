d0e00eddcb5b1feddacd032cadf2c843a1b75678
h4shcore <dakshgajbhiye902@gmail.com>
2026-02-26 02:11:22 +0530

    init

--- hosts/zeus/display.nix
+++ hosts/zeus/display.nix
@@ -19,6 +19,9 @@
 
   # Optional, hint Electron apps to use Wayland:
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  environment.variables = {
+    QT_QPA_PLATFORM = "wayland";
+  };
 
   # Enable Hyprland
   # programs.hyprland.enable = true;
