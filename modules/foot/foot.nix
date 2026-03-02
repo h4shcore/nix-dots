287f39f7294979f581e4a3bffb50f8b3b4a0578a
h4shcore <dakshgajbhiye902@gmail.com>
2026-03-03 02:05:16 +0530

    init

--- modules/foot/foot.nix
+++ modules/foot/foot.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ conf, ... }:
 {
   programs.foot = {
     enable = true;
@@ -9,6 +9,25 @@
         pad = "10x10 center";
       };
       scrollback.lines = 10000;
+      colors = with conf.colors.default; {
+        inherit foreground background;
+          regular0 = regular0;
+          regular1 = regular1;
+          regular2 = regular2;
+          regular3 = regular3;
+          regular4 = regular4;
+          regular5 = regular5;
+          regular6 = regular6;
+          regular7 = regular7;
+          bright0 = bright0;
+          bright1 = bright1;
+          bright2 = bright2;
+          bright3 = bright3;
+          bright4 = bright4;
+          bright5 = bright5;
+          bright6 = bright6;
+          bright7 = bright7;
+      };
     };
   };
 }
